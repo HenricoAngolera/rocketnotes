e4a2a1b6693abe84f71e5abda0359edbef8c38d3
henrico <henrico.an@gmail.com>
2023-04-21 15:48:40 -0300

    Fragment

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,10 @@
+import { Fragment } from "react";
+
 export function Details() {
   return(
-    <h1>Hello World!</h1>
+    <Fragment>
+      <h1>Hello World!</h1>
+      <span>Henrico Angoelra</span>
+    </Fragment>
   )
 }
