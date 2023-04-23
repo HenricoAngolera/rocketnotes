d4b8b1a4425220a05a3490cd748d1c5e33a5a02d
henrico <henrico.an@gmail.com>
2023-04-23 16:16:25 -0300

    component (button)

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,17 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/Button';
+
 export function Details() {
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Henrico Angolera</span>
+
+      <Button/>
+      <Button/>
+      <Button/>
+
     </Container>
   )
 }
