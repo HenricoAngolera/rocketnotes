c165c4c82ee933eefc712948f05c8368f4136324
henrico <henrico.an@gmail.com>
2023-04-26 17:00:32 -0300

    using images (bug)

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,7 @@ import { FiMail, FiLock } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-import { Container, Form } from "./styles";
+import { Container, Form, Background } from "./styles";
 
 export function SignIn(){
   return (
@@ -30,6 +30,7 @@ export function SignIn(){
           Criar Conta
         </a>
       </Form>
+      <Background />
     </Container>
   );
 }
