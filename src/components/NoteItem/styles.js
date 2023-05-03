71930f185d51b6db0d6f7fb95cacf0eed6ad4189
henrico <henrico.an@gmail.com>
2023-05-03 09:20:13 -0300

    styling noteitem

---
+++ src/components/NoteItem/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme, isNew }) => isNew ? "trasparent" : theme.COLORS.BACKGROUND_900}; /* Se for novo, background transparante, se não color Background */
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+  border: ${({ theme, isNew }) => isNew ? `1px dashed ${theme.COLORS.GRAY_300}` : "none"}; /* Se for novo tem borda, se não não tem */
+
+  margin-bottom: 8px;
+  border-radius: 10px;
+  padding-right: 16px;
+
+  > button {
+    border: none;
+    background: none;
+  }
+
+  .button-delete {
+    color: ${({ theme }) => theme.COLORS.RED};
+  }
+
+  .button-add {
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > input {
+    height: 56px;
+    width: 100%;
+
+    padding: 12px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    border: none;
+
+    &::placeholder {
+      color ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+  }
+`;
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
 
 import { Container, Form } from './styles';
 
@@ -23,6 +25,11 @@ export function New() {
           <TextArea 
             placeholder="Observações"
           />
+
+          <Section title="Links Úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
         </Form>
       </main>
     </Container>
