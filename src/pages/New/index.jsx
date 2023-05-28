bc45e99cb941792bc16405512f74d389ce33af8b
henrico <henrico.an@gmail.com>
2023-05-28 13:05:24 -0300

    register tags (in database)

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useNavigate } from 'react-router-dom';
+
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
@@ -8,14 +10,21 @@ import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 
+import { api } from '../../services/api';
+
 import { Container, Form } from './styles';
 
 export function New() {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState("")
 
   const [tags, setTags] = useState([])
   const [newTag, setNewTag] = useState("")
+
+  const navigate = useNavigate();
 
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink])
@@ -35,6 +44,18 @@ export function New() {
     setTags(prevState => prevState.filter(tag => tag !== deletedTag))
   }
 
+  async function handleNewNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    })
+
+    alert("Note created with success!")
+    navigate("/")
+  }
+
   return(
     <Container>
       <Header />
@@ -47,10 +68,12 @@ export function New() {
           </header>
 
           <Input
+            onChange={e => setTitle(e.target.value)}
             placeholder="Título"
           />
 
           <TextArea 
+            onChange={e => setDescription(e.target.value)}
             placeholder="Observações"
           />
 
@@ -94,7 +117,10 @@ export function New() {
               />
             </div>
           </Section>
-          <Button title="Salvar" />
+          <Button 
+            title="Salvar" 
+            onClick={handleNewNote}
+          />
         </Form>
       </main>
     </Container>
