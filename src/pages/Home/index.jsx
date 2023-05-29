0b89215f273d253e508ef990dbc1fc96e842d2cd
henrico <henrico.an@gmail.com>
2023-05-29 15:50:30 -0300

    loading tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
 import { FiPlus } from 'react-icons/fi';
 
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { api } from '../../services/api';
 
 import { Note } from '../../components/Note';
 import { Header } from '../../components/Header';
@@ -9,6 +12,16 @@ import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
+  const [tags, setTags] = useState([])
+  
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
   return (
     <Container>
       <Brand>
@@ -18,10 +31,21 @@ export function Home(){
       <Header />
 
       <Menu>
-        <li><ButtonText title='Todos' isActive/></li>
-        <li><ButtonText title='React' /></li>
-        <li><ButtonText title='Nodejs' /></li>
-
+        <li>
+          <ButtonText 
+            title='Todos' 
+            isActive
+          />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name} 
+              />
+            </li>
+          ))
+        }
       </Menu>
       <Search>
         <Input placeholder="Pesquisar pelo título" />
